45258fcfc8c497435f8931b31255860b223e6336
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 10:16:53 -0600

    add wiki command

---
+++ src/commands/wiki.js
@@ -0,0 +1,16 @@
+const Discord = require(`discord.js`);
+const config = require(`../../config/config.js`);
+
+module.exports = {
+    desc: `View the wiki.`
+};
+
+module.exports.run = async (client, message, args) => {
+    const sEmbed = new Discord.RichEmbed()
+        .setColor(0x663399)
+        .setAuthor(`Torn.Space Wiki`, message.author.avatarURL, `https://tornspace.fandom.com/`)
+        .setDescription(`Click here to view the [Torn.Space](https://torn.space) wiki.`)
+        .setTimestamp(new Date())
+        .setFooter(config.footer);
+    return message.channel.send(sEmbed);
+};
