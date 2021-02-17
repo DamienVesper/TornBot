29a92be53a8d683d76539f02f21bd2080b0d188a
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 10:38:15 -0600

    feeling dumber

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 const log = require(`../utils/log.js`);
 
 module.exports = client => {
-    client.on(`ready`, async () => log(`green`, `${client.user.username}#${client.user.discriminator} has started, with ${client.users.size} users in ${client.guilds.size} servers.`));
+    log(`green`, `${client.user.username}#${client.user.discriminator} has started, with ${client.users.size} users in ${client.guilds.size} servers.`);
 };
