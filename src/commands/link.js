97b5eba3e9fef5ae6312c97394178e6fb05184e2
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 19:22:37 -0600

    priority messages

--- src/commands/link.js
+++ src/commands/link.js
@@ -13,11 +13,11 @@ module.exports.run = async (client, message, args) => {
 
     const userToLink = args[0].toString().toLowerCase();
 
-    const accountIsRegistered = await User.findOne({ accountName: userToLink });
     const userIsRegistered = await User.findOne({ discordID: message.author.id });
+    const accountIsRegistered = await User.findOne({ accountName: userToLink });
 
-    if (accountIsRegistered) return message.channel.send(`${m} That account has already been registered!`);
-    else if (userIsRegistered) return message.channel.send(`${m} You are already linked to an account!`);
+    if (userIsRegistered) return message.channel.send(`${m} You are already linked to an account!`);
+    else if (accountIsRegistered) return message.channel.send(`${m} That account has already been registered!`);
     else if (!tornUsers.find(user => user.username === userToLink)) return message.channel.send(`${m} That account does not exist!`);
 
     const user = new User({
