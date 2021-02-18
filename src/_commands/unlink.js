559f0edd1d475528b1d8cbd1caa8286f57877d34
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 20:00:03 -0600

    Revert "remove old commands"
    This reverts commit c60fd53f0baa7b9d9b9434324fc99f6d4d7ee41e.

---
+++ src/_commands/unlink.js
@@ -0,0 +1,18 @@
+const User = require(`../models/user.model`);
+
+module.exports = {
+    name: `unlink`,
+    description: `Unlink your Torn account from that of your Discord.`,
+    usage: null
+};
+
+module.exports.run = async (client, message, args) => {
+    if (message.author.id === `448619102198693889`) return;
+
+    const account = await User.findOne({ discordID: message.author.id });
+    if (!account) return message.channel.send(`${message.author} You are not linked to an account!`);
+
+    User.deleteOne({ discordID: message.author.id })
+        .catch(() => message.channel.send(`${message.author} Failed to unlink your account. Please contact a developer.`))
+        .then(() => message.channel.send(`${message.author} Succesfully unlinked your account.`));
+};
---
+++ src/_commands/weapon.js
@@ -0,0 +1,33 @@
+const Discord = require(`discord.js`);
+const User = require(`../models/user.model`);
+const {
+    config
+} = require(`../index.js`);
+
+module.exports = {
+    name: `weapon`,
+    description: `View a weapon.`,
+    usage: `[item]`
+};
+
+module.exports.run = async (client, message, args) => {
+    const weapons = require(`../../config/weapons.json`);
+    const weapon = weapons[args[0] === undefined ? 0 : args[0]];
+
+    const sEmbed = new Discord.RichEmbed()
+        .setAuthor(`${weapon.type} | ${weapon.name}`, message.author.avatarURL)
+        .setColor(0xffa500)
+        .setDescription(`
+This weapon ${weapon.bot ? `seeks` : `does not seek`} other ships.
+
+Damage: ${weapon.damage}
+Price: ${weapon.price}
+Charge: ${weapon.charge}
+
+Range: ${weapon.range}
+Speed: ${weapon.speed} 
+`)
+        .setTimestamp(new Date())
+        .setFooter(config.footer);
+    return message.channel.send(sEmbed);
+};
