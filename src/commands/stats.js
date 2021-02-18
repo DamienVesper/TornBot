285a1b6856492a814d4ac69e3000eacd5f621ba5
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 19:23:39 -0600

    fix xp

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -23,7 +23,7 @@ module.exports.run = async (client, message, args) => {
     const sEmbed = new Discord.RichEmbed()
         .setAuthor(`#${tornUser.placement} | ${tornUser.displayName}`, client.user.avatarURL)
         .setColor(tornUser.team === `Cyborg` ? 0x32cd32 : tornUser.team === `Alien` ? 0xffc0cb : tornUser.team === `Human` ? 0x00b7eb : 0x00000)
-        .setDescription(`**Rank**: ${tornUser.rank}\n**Experience**: ${tornUser.experience}\n**Kills**: ${tornUser.kills}\n**Money**:  ${tornUser.money}\n**Tech**: ${tornUser.tech}`)
+        .setDescription(`**Rank**: ${tornUser.rank}\n**Experience**: ${tornUser.xp}\n**Kills**: ${tornUser.kills}\n**Money**:  ${tornUser.money}\n**Tech**: ${tornUser.tech}`)
         .setTimestamp(new Date())
         .setFooter(config.footer);
 
