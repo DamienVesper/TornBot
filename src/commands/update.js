52f8492ac6c6180c2139becf5387e5e6811f80fe
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 20:24:17 -0600

    get torn users

---
+++ src/commands/update.js
@@ -0,0 +1,43 @@
+const Discord = require(`discord.js`);
+const config = require(`../../config/config.js`);
+
+module.exports = {
+    desc: `Update a user's roles.`,
+    usage: `[command]`
+};
+
+module.exports.run = async (client, message, args) => {
+    const m = `${message.author} »`;
+
+    const commands = client.commands;
+    const data = [];
+
+    if (!args[0]) {
+        let helpTxt = ``;
+        commands.forEach(cmd => helpTxt += `\`${config.prefix + cmd.name + (cmd.usage ? ` ${cmd.usage}` : ``)}\` - ${cmd.desc}\n`);
+
+        const sEmbed = new Discord.RichEmbed()
+            .setColor(0xcfcf53)
+            .setAuthor(`Help Menu`)
+            .setDescription(helpTxt)
+            .setTimestamp(new Date())
+            .setFooter(config.footer);
+        return message.channel.send(sEmbed);
+    }
+
+    const commandName = args[0].toLowerCase();
+    const command = commands.find(command => command.name === commandName) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+    if (!command) return message.channel.send(`${m} That is not a valid command!`);
+
+    if (command.usage) data.push(`**Usage:** \`${config.prefix}${command.name} ${command.usage}\``);
+    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(`, `)}`);
+
+    const sEmbed = new Discord.RichEmbed()
+        .setColor(0xcfcf53)
+        .setAuthor(`Help Menu | ${command.name.slice(0, 1).toUpperCase() + command.name.slice(1)}`)
+        .setDescription(`${command.desc}\n\n${data.join(`\n`)}`)
+        .setTimestamp(new Date())
+        .setFooter(config.footer);
+    return message.channel.send(sEmbed);
+};
---
+++ src/utils/updateRoles.js
@@ -0,0 +1,30 @@
+const getTornUsers = require(`./getTornUsers.js`);
+const log = require(`./log.js`);
+
+module.exports = async (client, member, tornUser) => {
+    const tornUsers = await getTornUsers();
+    log(`magenta`, `Updating roles for ${tornUser}.`);
+
+    const curUser = tornUsers.find(user => user.username === tornUser);
+
+    const placementRoles = [`453678967996678145`, `453678938275708960`, `453678890628546560`, `453678855534804992`, `453612904365948929`, `453620521632923660`, `453620581041045555`, `453620631116709888`, `453620675526000674`, `453620720581214208`];
+    const teamRoles = [`513781861542002690`, `524288679473184806`, `633664409528565798`];
+    const accountRoles = [`723239225855705198`];
+
+    member.roles.forEach(role => (placementRoles.includes(role.id) || teamRoles.includes(role.id) || accountRoles.includes(role.id)) ? member.removeRole(role.id) : null);
+
+    if (curUser.placement <= 5) member.addRole(placementRoles[9]);
+    else if (curUser.placement <= 10) member.addRole(placementRoles[8]);
+    else if (curUser.placement <= 25) member.addRole(placementRoles[7]);
+    else if (curUser.placement <= 50) member.addRole(placementRoles[6]);
+    else if (curUser.placement <= 75) member.addRole(placementRoles[5]);
+    else if (curUser.placement <= 100) member.addRole(placementRoles[4]);
+    else if (curUser.placement <= 250) member.addRole(placementRoles[3]);
+    else if (curUser.placement <= 500) member.addRole(placementRoles[2]);
+    else if (curUser.placement <= 750) member.addRole(placementRoles[1]);
+    else if (curUser.placement <= 1000) member.addRole(placementRoles[0]);
+
+    if (curUser.team === `Human`) member.addRole(teamRoles[0]);
+    else if (curUser.team === `Alien`) member.addRole(teamRoles[1]);
+    else member.addRole(teamRoles[2]);
+};
