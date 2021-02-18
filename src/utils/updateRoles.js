a4b04efa1ead6adcc8ffc24ef04e4efcb2ada57a
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 21:47:50 -0600

    await works

--- src/utils/updateRoles.js
+++ src/utils/updateRoles.js
@@ -9,24 +9,24 @@ module.exports = async (client, member, tornUser, tornUsers) => {
     const teamRoles = [`513781861542002690`, `524288679473184806`, `633664409528565798`];
     const accountRoles = [`723239225855705198`];
 
-    member.roles.forEach(role => (placementRoles.includes(role.id) || teamRoles.includes(role.id)) ? member.removeRole(role.id) : null);
-    member.addRole(accountRoles[0]);
+    member.roles.forEach(async role => (placementRoles.includes(role.id) || teamRoles.includes(role.id)) ? await member.removeRole(role.id) : null);
+    await member.addRole(accountRoles[0]);
 
-    setTimeout(() => {
-        if (curUser.placement <= 5) member.addRole(placementRoles[9]);
-        else if (curUser.placement <= 10) member.addRole(placementRoles[8]);
-        else if (curUser.placement <= 25) member.addRole(placementRoles[7]);
-        else if (curUser.placement <= 50) member.addRole(placementRoles[6]);
-        else if (curUser.placement <= 75) member.addRole(placementRoles[5]);
-        else if (curUser.placement <= 100) member.addRole(placementRoles[4]);
-        else if (curUser.placement <= 250) member.addRole(placementRoles[3]);
-        else if (curUser.placement <= 500) member.addRole(placementRoles[2]);
-        else if (curUser.placement <= 750) member.addRole(placementRoles[1]);
-        else if (curUser.placement <= 1000) member.addRole(placementRoles[0]);
+    setTimeout(async () => {
+        if (curUser.placement <= 5) await member.addRole(placementRoles[9]);
+        else if (curUser.placement <= 10) await member.addRole(placementRoles[8]);
+        else if (curUser.placement <= 25) await member.addRole(placementRoles[7]);
+        else if (curUser.placement <= 50) await member.addRole(placementRoles[6]);
+        else if (curUser.placement <= 75) await member.addRole(placementRoles[5]);
+        else if (curUser.placement <= 100) await member.addRole(placementRoles[4]);
+        else if (curUser.placement <= 250) await member.addRole(placementRoles[3]);
+        else if (curUser.placement <= 500) await member.addRole(placementRoles[2]);
+        else if (curUser.placement <= 750) await member.addRole(placementRoles[1]);
+        else if (curUser.placement <= 1000) await member.addRole(placementRoles[0]);
 
-        if (curUser.team === `Human`) member.addRole(teamRoles[0]);
-        else if (curUser.team === `Alien`) member.addRole(teamRoles[1]);
-        else member.addRole(teamRoles[2]);
+        if (curUser.team === `Human`) await member.addRole(teamRoles[0]);
+        else if (curUser.team === `Alien`) await member.addRole(teamRoles[1]);
+        else await member.addRole(teamRoles[2]);
 
         log(`blue`, `Roles updated for ${tornUser}.`);
     }, 2e3);
