cd73cc9b6bff794e120b7ae134f85b12b1787467
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 19:38:34 -0600

    proper handler and fix wiki

--- src/commands/wiki.js
+++ src/commands/wiki.js
@@ -8,8 +8,8 @@ module.exports = {
 module.exports.run = async (client, message, args) => {
     const sEmbed = new Discord.RichEmbed()
         .setColor(0x663399)
-        .setAuthor(`Torn.Space Wiki`, message.author.avatarURL, `https://tornspace.fandom.com/`)
-        .setDescription(`Click here to view the [Torn.Space](https://torn.space) wiki.`)
+        .setAuthor(`Torn.Space Wiki`, message.author.avatarURL, `https://tornspace.fandom.com`)
+        .setDescription(`Click [here](https://tornspace.fandom.com) to view the Torn.Space wiki.`)
         .setTimestamp(new Date())
         .setFooter(config.footer);
     return message.channel.send(sEmbed);
