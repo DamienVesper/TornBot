6f2cb1c93f931f5146932e81b8e70436b2c5e6f5
Damien Vesper <ldamienvesper@gmail.com>
2021-02-17 20:29:44 -0600

    start an update command

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,43 +1,16 @@
-const Discord = require(`discord.js`);
-const config = require(`../../config/config.js`);
+const User = require(`../models/user.model.js`);
+const updateRoles = require(`../utils/updateRoles.js`);
 
 module.exports = {
-    desc: `Update a user's roles.`,
-    usage: `[command]`
+    desc: `Update your roles.`
 };
 
 module.exports.run = async (client, message, args) => {
     const m = `${message.author} »`;
 
-    const commands = client.commands;
-    const data = [];
+    const dbUser = await User.findOne({ discordID: message.author.id });
+    if (!dbUser) return message.channel.send(`${m} You don't have an account yet!`);
 
-    if (!args[0]) {
-        let helpTxt = ``;
-        commands.forEach(cmd => helpTxt += `\`${config.prefix + cmd.name + (cmd.usage ? ` ${cmd.usage}` : ``)}\` - ${cmd.desc}\n`);
-
-        const sEmbed = new Discord.RichEmbed()
-            .setColor(0xcfcf53)
-            .setAuthor(`Help Menu`)
-            .setDescription(helpTxt)
-            .setTimestamp(new Date())
-            .setFooter(config.footer);
-        return message.channel.send(sEmbed);
-    }
-
-    const commandName = args[0].toLowerCase();
-    const command = commands.find(command => command.name === commandName) || commands.find(c => c.aliases && c.aliases.includes(commandName));
-
-    if (!command) return message.channel.send(`${m} That is not a valid command!`);
-
-    if (command.usage) data.push(`**Usage:** \`${config.prefix}${command.name} ${command.usage}\``);
-    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(`, `)}`);
-
-    const sEmbed = new Discord.RichEmbed()
-        .setColor(0xcfcf53)
-        .setAuthor(`Help Menu | ${command.name.slice(0, 1).toUpperCase() + command.name.slice(1)}`)
-        .setDescription(`${command.desc}\n\n${data.join(`\n`)}`)
-        .setTimestamp(new Date())
-        .setFooter(config.footer);
-    return message.channel.send(sEmbed);
+    updateRoles(client, message.member, dbUser.accountName);
+    message.channel.send(`${m} Updating roles...`);
 };
