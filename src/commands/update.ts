671dc5c97142e798efdb973af8d445697af95dc1
Damien Vesper <ldamienvesper@gmail.com>
2021-04-02 00:36:54 -0500

    update cmd

---
+++ src/commands/update.ts
@@ -0,0 +1,23 @@
+import { User } from '../models/user.model';
+
+import * as Discord from 'discord.js';
+import { Client } from '../index';
+
+import { tornAccount, getTornUsers } from '../utils/getTornUsers';
+import { updateRoles } from '../utils/updateRoles';
+
+export default {
+    desc: `Update your roles.`,
+    aliases: [`h`, `?`]
+};
+
+export const run = async (client: Client, message: Discord.Message, args: any[]) => {
+    const tornUsers: tornAccount[] = await getTornUsers();
+    const m = `${message.author} »`;
+
+    const dbUser = await User.findOne({ discordID: message.author.id });
+    if (!dbUser) return message.channel.send(`${m} You don't have an account yet!`);
+
+    updateRoles(client, message.member, dbUser.accountName, tornUsers);
+    message.channel.send(`${m} Updating roles...`);
+};
