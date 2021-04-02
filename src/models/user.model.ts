0cabfeecec883476a07f71e95e4d29d7c8f34864
Damien Vesper <ldamienvesper@gmail.com>
2021-04-02 00:01:12 -0500

    ts model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,4 @@
-const mongoose = require(`mongoose`);
+import * as mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     banned: {
@@ -21,4 +21,4 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model(`User`, userSchema);
+export default mongoose.model(`User`, userSchema);
