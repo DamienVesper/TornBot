f42ecc615fb987730a99c81d31e103f623ef6934
Damien Vesper <ldamienvesper@gmail.com>
2021-06-01 19:24:18 -0500

    better tsconf

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -1,7 +1,7 @@
 import config from '../../config/config';
 
 import * as Discord from 'discord.js';
-import { Client } from '../index';
+import { Client } from '../types/discord';
 
 import User from '../models/user.model';
 import { tornAccount, getTornUsers } from '../utils/getTornUsers';
