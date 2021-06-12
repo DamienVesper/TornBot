b3f1a37bc05956e0223b324def266699181a11c4
Damien Vesper <ldamienvesper@gmail.com>
2021-06-12 10:45:19 -0500

    clean up ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -7,9 +7,8 @@ import refreshActivity from '../utils/refreshActivity';
 export default async (client: Client) => {
     log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
 
-    logHeader(() => {
-        refreshActivity(client, () => {
-            setInterval(() => refreshActivity(client), 6e5);
-        });
-    });
+    logHeader();
+    await refreshActivity(client);
+
+    setInterval(async () => (await refreshActivity(client)), 6e5);
 };
