d0e812e4dec8ffd6d501208e42a460be00131747
Damien Vesper <ldamienvesper@gmail.com>
2021-06-12 11:01:45 -0500

    update the leaderboard every 25 min

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,14 +1,18 @@
 import { Client } from '../types/discord';
+
 import log from '../utils/log';
 import { logHeader } from '../utils/logExtra';
 
 import refreshActivity from '../utils/refreshActivity';
+import updateLeaderboard from '../modules/updateLeaderboard';
 
 export default async (client: Client) => {
     log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
-
     logHeader();
+
     await refreshActivity(client);
+    await updateLeaderboard();
 
     setInterval(async () => (await refreshActivity(client)), 6e5);
+    setInterval(async () => (await updateLeaderboard()), 15e5);
 };
