697144f05ed3c319a26e7da6dec4712b0b3abe18
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 17:51:52 -0600

    enhance: strongtype query grabber

--- src/utils/getQuery.ts
+++ src/utils/getQuery.ts
@@ -2,13 +2,13 @@ import * as Discord from 'discord.js';
 
 import User from '../models/user.model';
 
-const getQuery = async (interaction: Discord.CommandInteraction, username: string): Promise<string> => {
-    if (!username) {
+const getQuery = async (interaction: Discord.CommandInteraction, username: string | undefined): Promise<string | undefined> => {
+    if (username === undefined) {
         username = (await User.findOne({ discordID: interaction.user.id }))?.accountName;
-        if (!username) return undefined;
+        if (username === undefined) return undefined;
     } else {
         const dbUsername = (await User.findOne({ accountName: username }))?.accountName;
-        if (dbUsername) username = dbUsername;
+        if (dbUsername === undefined) username = dbUsername;
         else {
             const dbIDUsername = (await User.findOne({
                 discordID:
@@ -16,7 +16,7 @@ const getQuery = async (interaction: Discord.CommandInteraction, username: strin
                         ? username.slice(3, -1)
                         : username
             }))?.accountName;
-            if (dbIDUsername) username = dbIDUsername;
+            if (dbIDUsername === undefined) username = dbIDUsername;
         }
     }
 
