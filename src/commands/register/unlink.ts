6ae029c4276e77a84bf6a4668a9ea4b169fdea49
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 13:08:36 -0600

    whatever

--- src/commands/register/unlink.ts
+++ src/commands/register/unlink.ts
@@ -15,22 +15,6 @@ const run = async (client: Client, interaction: Discord.CommandInteraction) => {
     const account = await User.findOne({ discordID: interaction.user.id });
     if (!account) return interaction.reply({ content: `You are not linked to an account!`, ephemeral: true });
 
-    const sRow = new Discord.MessageActionRow().addComponents(
-        new Discord.MessageButton().setCustomId(`confirmation-yes`).setLabel(`Delete`).setStyle(`DANGER`),
-        new Discord.MessageButton().setCustomId(`confirmation-no`).setLabel(`Keep`).setStyle(`PRIMARY`)
-    );
-
-    const sEmbed = new Discord.MessageEmbed()
-        .setColor(0x1e90ff)
-        .setAuthor(`Are you sure you want to do this?`)
-        .setDescription(`By confirming, you are agreeing to destroy all your data within the application.`)
-        .setTimestamp(new Date())
-        .setFooter(config.footer);
-
-    await interaction.reply({ embeds: [sEmbed], components: [sRow] });
-
-    const collector = interaction.channel.createMessageComponentCollector({ filter: , time: 15000 });
-
     User.deleteOne({ discordID: interaction.user.id })
         .catch(() => interaction.reply(`Failed to unlink your account. Please contact a developer.`))
         .then(() => interaction.reply(`Succesfully unlinked your account.`));
