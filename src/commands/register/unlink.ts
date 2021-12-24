95d5f23df350a41986125fdde3b2f7d9cedaa2c8
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 17:52:36 -0600

    enhance: strongtype unlink cmd

--- src/commands/register/unlink.ts
+++ src/commands/register/unlink.ts
@@ -9,11 +9,11 @@ const cmd: SlashCommandBuilder = new SlashCommandBuilder()
     .setName(`unlink`)
     .setDescription(`Unlink your Torn account from Discord.`);
 
-const run = async (client: Client, interaction: Discord.CommandInteraction) => {
+const run = async (client: Client, interaction: Discord.CommandInteraction): Promise<void> => {
     const account = await User.findOne({ discordID: interaction.user.id });
-    if (account == null) return await interaction.reply({ content: `You are not linked to an account!`, ephemeral: true });
+    if (account === undefined) return await interaction.reply({ content: `You are not linked to an account!`, ephemeral: true });
 
-    User.deleteOne({ discordID: interaction.user.id })
+    await User.deleteOne({ discordID: interaction.user.id })
         .catch(async () => await interaction.reply(`Failed to unlink your account. Please contact a developer.`))
         .then(async () => await interaction.reply(`Succesfully unlinked your account.`));
 };
