2d0308b23a80f3df5c4aa132059fc048832b1971
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 13:02:18 -0600

    wiki: cmd

--- src/commands/guides/wiki.ts
+++ src/commands/guides/wiki.ts
@@ -1,20 +1,27 @@
-import * as Discord from 'discord.js';
 import config from '../../../config/config';
 
-import { Client, CommandConfig } from '../../typings/discord';
+import * as Discord from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
 
-const cmd: CommandConfig = {
-    desc: `View the wiki.`
-};
+import { Client } from '../../typings/discord';
 
-const run = async (client: Client, message: Discord.Message, args: string[]) => {
+const cmd: SlashCommandBuilder = new SlashCommandBuilder()
+    .setName(`wiki`)
+    .setDescription(`View the wiki.`);
+
+const run = async (client: Client, interaction: Discord.CommandInteraction) => {
     const sEmbed: Discord.MessageEmbed = new Discord.MessageEmbed()
         .setColor(config.colors.purple)
-        .setAuthor(`Torn.Space Wiki`, message.author.avatarURL(), `https://tornspace.fandom.com`)
+        .setAuthor({
+            name: `Torn.Space Wiki`,
+            iconURL: interaction.user.avatarURL(),
+            url: `https://tornspace.fandom.com`
+        })
         .setDescription(`Click [here](https://tornspace.fandom.com) to view the Torn.Space wiki.`)
         .setTimestamp(new Date())
         .setFooter(config.footer);
-    message.reply({ embeds: [sEmbed] });
+
+    await interaction.reply({ embeds: [sEmbed] });
 };
 
 export {
