b88b375d22ed5f48d4a6af896da727a27a70b0b3
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 17:51:57 -0600

    enhance: strongtype command deployer

--- src/utils/deployCommands.ts
+++ src/utils/deployCommands.ts
@@ -7,12 +7,19 @@ import log from './log';
 import { Client } from '../typings/discord';
 
 const deployCommands = async (client: Client): Promise<void> => {
+    if (process.env.DISCORD_TOKEN === undefined ||
+        process.env.DISCORD_ID === undefined ||
+        process.env.GUILD_ID === undefined ||
+        client?.commands === undefined) return;
+
     const rest = new REST({ version: `9` }).setToken(process.env.DISCORD_TOKEN);
 
-    const commands: Object[] = [];
+    const commands: Array<Record<string, unknown>> = [];
+
     for (const [name, command] of client.commands) {
         log(`yellow`, `Registering command ${name}...`);
-        commands.push(command.cmd.toJSON());
+
+        commands.push(((command.cmd.toJSON() as unknown) as Record<string, unknown>));
     }
 
     await rest.put(Routes.applicationGuildCommands(process.env.DISCORD_ID, process.env.GUILD_ID), { body: commands })
