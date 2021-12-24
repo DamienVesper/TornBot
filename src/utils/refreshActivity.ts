12314d1103ffe95e7a4db89547ce8502a99892cf
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 17:51:14 -0600

    strongtype refreshactivity

--- src/utils/refreshActivity.ts
+++ src/utils/refreshActivity.ts
@@ -5,10 +5,10 @@ import log from '../utils/log';
  * Refresh the activity of the client.
  * @param client The Discord client to use.
  */
-const refreshActivity = async (client: Client) => {
+const refreshActivity = async (client: Client): Promise<void> => {
     log(`cyan`, `Updating status...`);
 
-    client.user.setPresence({
+    client?.user?.setPresence({
         activities: [{
             name: `Torn.Space`,
             type: `WATCHING`
