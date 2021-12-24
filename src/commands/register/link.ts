3a68676b7bf2a718c59c3ebe8754fa2a15378286
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 13:02:09 -0600

    link cmd

--- src/commands/register/link.ts
+++ src/commands/register/link.ts
@@ -1,34 +1,35 @@
 import * as Discord from 'discord.js';
-
-import { Client, CommandConfig } from '../../typings/discord';
-import { TornAccount } from '../../typings/accounts';
+import { SlashCommandBuilder } from '@discordjs/builders';
 
 import User from '../../models/user.model';
 import Leaderboard from '../../models/leaderboard.model';
 
-const cmd: CommandConfig = {
-    desc: `Link your Torn account to Discord.`,
-    usage: `<user>`
-};
+import { Client } from '../../typings/discord';
+import { TornAccount } from '../../typings/accounts';
 
-const run = async (client: Client, message: Discord.Message, args: string[]) => {
+const cmd: Omit<SlashCommandBuilder, `addSubcommand` | `addSubcommandGroup`> = new SlashCommandBuilder()
+    .setName(`link`)
+    .setDescription(`Link your Torn account to Discord.`)
+    .addStringOption(option => option.setName(`account`).setDescription(`The account you want to link.`));
+
+const run = async (client: Client, interaction: Discord.CommandInteraction) => {
     const tornUsers: Map<string, TornAccount> = (await Leaderboard.findOne()).accounts;
 
-    const userToLink = args[0].toString().toLowerCase();
+    const userToLink = interaction.options.getString(`account`).toLowerCase();
 
-    const userIsRegistered = await User.findOne({ discordID: message.author.id });
+    const userIsRegistered = await User.findOne({ discordID: interaction.user.id });
     const accountIsRegistered = await User.findOne({ accountName: userToLink });
 
-    if (userIsRegistered) return message.reply(`You are already linked to an account!`);
-    else if (accountIsRegistered) return message.reply(`That account has already been registered!`);
-    else if (!tornUsers.get(userToLink)) return message.reply(`That account does not exist!`);
+    if (userIsRegistered) return interaction.reply({ content: `You are already linked to an account!`, ephemeral: true });
+    else if (accountIsRegistered) return interaction.reply({ content: `That account has already been registered!`, ephemeral: true });
+    else if (!tornUsers.get(userToLink)) return interaction.reply({ content: `That account does not exist!`, ephemeral: true });
 
     const user = new User({
         accountName: userToLink,
-        discordID: message.author.id
+        discordID: interaction.user.id
     });
 
-    user.save(() => message.reply(`You are now linked to account \`${userToLink}\`.`));
+    user.save(() => interaction.reply(`You are now linked to account \`${userToLink}\`.`));
 };
 
 export {
