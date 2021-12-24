1c0fc9f536b6443650ee208c095043271e084939
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 17:52:15 -0600

    enhance: strongtype ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,13 +6,13 @@ import { logHeader } from '../utils/logExtra';
 import refreshActivity from '../utils/refreshActivity';
 import updateLeaderboard from '../modules/updateLeaderboard';
 
-export default async (client: Client) => {
+export default async (client: Client): Promise<void> => {
     log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
     logHeader();
 
     await refreshActivity(client);
     await updateLeaderboard();
 
-    setInterval(async () => (await refreshActivity(client)), 6e5);
-    setInterval(async () => (await updateLeaderboard()), 15e5);
+    setInterval(() => { void refreshActivity(client); }, 6e5);
+    setInterval(() => { void updateLeaderboard(); }, 15e5);
 };
