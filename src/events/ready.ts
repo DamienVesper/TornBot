4266fede1c04849bcef4bf399637d46149fa0dc5
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 13:17:48 -0600

    Revert "ready evt rewrite"
    This reverts commit 84f362bf3fbf0a354ae4ba4beefeebd2f6b6ca98.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,16 +6,13 @@ import { logHeader } from '../utils/logExtra';
 import refreshActivity from '../utils/refreshActivity';
 import updateLeaderboard from '../modules/updateLeaderboard';
 
-export default {
-    event: `ready`,
-    run: async (client: Client) => {
-        log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
-        logHeader();
+export default async (client: Client) => {
+    log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
+    logHeader();
 
-        await refreshActivity(client);
-        await updateLeaderboard();
+    await refreshActivity(client);
+    await updateLeaderboard();
 
-        setInterval(async () => (await refreshActivity(client)), 6e5);
-        setInterval(async () => (await updateLeaderboard()), 15e5);
-    }
+    setInterval(async () => (await refreshActivity(client)), 6e5);
+    setInterval(async () => (await updateLeaderboard()), 15e5);
 };
