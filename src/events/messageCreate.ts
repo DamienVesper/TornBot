2d888460093a6de0ea8fc417ed672ef48ba3ca60
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 12:17:15 -0600

    modify message event for slash commands

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,30 +1,18 @@
 import * as Discord from 'discord.js';
 import { Client } from '../typings/discord';
 
-import config from '../../config/config';
 import log from '../utils/log';
 
-export default async (client: Client, message: Discord.Message) => {
-    const m = `${message.author} »`;
-
-    // Botception and prefix handling.
-    if (message.author.bot || message.channel.type === `DM`) return;
-    if (message.content.slice(0, config.prefix.length).toString().toLowerCase() !== config.prefix) return;
-
-    // Parse arguments and command.
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+export default async (client: Client, interaction: Discord.Interaction) => {
+    if (!interaction.isCommand()) return;
 
     // Grab the command from the handler.
-    const cmd = client.commands.get(command) ||
-        client.commands.get([...client.commands.keys()][[...client.commands.values()].indexOf([...client.commands.values()].find(cmd => cmd.config.aliases?.includes(command)))]);
+    const cmd = client.commands.get(interaction.commandName);
 
+    // If the command doesn't exist, return.
     if (!cmd) return;
 
-    if ((cmd.config.usage) && args.length < (cmd.config.usage.split(`<`).length) - 1) return message.channel.send(`${m} Proper usage is \`${config.prefix + command} ${cmd.config.usage}\`.`);
-    else {
-        // Execute the command.
-        log(`magenta`, `${message.author.tag} [${message.author.id}] ran command ${command} in ${message.guild.name}.`);
-        cmd.run(client, message, args);
-    }
+    // Execute the command.
+    log(`magenta`, `${interaction.user.tag} [${interaction.user.id}] ran command ${interaction.commandName} in ${interaction.guild.name}.`);
+    cmd.run(client, interaction);
 };
