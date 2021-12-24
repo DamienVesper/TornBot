7b1893561f8778ccc9b491afc674919adc9f0dbe
Damien Vesper <ldamienvesper@gmail.com>
2021-12-24 17:52:26 -0600

    enhance: strongtype update cmd

--- src/commands/stats/update.ts
+++ src/commands/stats/update.ts
@@ -8,19 +8,20 @@ import updateRoles from '../../utils/updateRoles';
 
 import { Client } from '../../typings/discord';
 import { TornAccount } from '../../typings/accounts';
+import { LeaderboardDoc } from '../../typings/models';
 
 const cmd: SlashCommandBuilder = new SlashCommandBuilder()
     .setName(`update`)
     .setDescription(`Update your roles.`);
 
-const run = async (client: Client, interaction: Discord.CommandInteraction) => {
-    const tornUsers: Map<string, TornAccount> = (await Leaderboard.findOne()).accounts;
+const run = async (client: Client, interaction: Discord.CommandInteraction): Promise<void> => {
+    const tornUsers: Map<string, TornAccount> = ((await Leaderboard.findOne()) as LeaderboardDoc)?.accounts;
 
     const dbUser = await User.findOne({ discordID: interaction.user.id });
     if (dbUser == null) return await interaction.reply({ content: `You don't have an account yet!`, ephemeral: true });
 
-    interaction.reply({ content: `Updating roles...`, ephemeral: true });
-    await updateRoles((await interaction.guild.members.fetch(interaction.user.id)), dbUser.accountName, tornUsers);
+    await interaction.reply({ content: `Updating roles...`, ephemeral: true });
+    await updateRoles((await (interaction.guild as Discord.Guild)?.members.fetch(interaction.user.id)), dbUser.accountName, tornUsers);
 };
 
 export {
